40b69edce4ebeb7331c569e612785bf03edbbce8
Shmidt Anton <sparda89328490316@mail.ru>
2024-10-03 15:02:22 +0500

    000

---
+++ source/repos/Baklashka_Toxi/Baklashka_Toxi/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace Baklashka_Toxi
+    {
+        public class Utilities
+        {
+            // Функция сложения двух чисел
+            public int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            // Функция вычитания двух чисел
+            public int Subtract(int a, int b)
+            {
+                return a - b;
+            }
+
+            // Функция умножения двух чисел
+            public int Multiply(int a, int b)
+            {
+                return a * b;
+            }
+
+            // Функция деления двух чисел
+            public double Divide(double a, double b)
+            {
+                if (b == 0)
+                {
+                    throw new DivideByZeroException("Деление на ноль недопустимо.");
+                }
+                return a / b;
+            }
+
+            // Функция конкатенации двух строк
+            public string ConcatenateStrings(string str1, string str2)
+            {
+                return str1 + str2;
+            }
+        }
+    }
---
+++ source/repos/Baklashka_Toxi/Baklashka_Toxi/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Baklashka_Toxi
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем объект класса Utilities из библиотеки
+            Utilities utils = new Utilities();
+
+            // Примеры вызова функций
+            int sum = utils.Add(5, 3);
+            Console.WriteLine($"Сумма: {sum}");
+
+            int difference = utils.Subtract(10, 4);
+            Console.WriteLine($"Разность: {difference}");
+
+            int product = utils.Multiply(7, 6);
+            Console.WriteLine($"Произведение: {product}");
+
+            try
+            {
+                double quotient = utils.Divide(20, 4);
+                Console.WriteLine($"Частное: {quotient}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            string concatenated = utils.ConcatenateStrings("Hello, ", "World!");
+            Console.WriteLine($"Конкатенация строк: {concatenated}");
+        }
+    }
+}
